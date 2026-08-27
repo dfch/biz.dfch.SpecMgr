fe9bfc5e998fcdbc3c5badda60db88eb4d8668da
Ronald Rink <dfch@users.noreply.github.com>
2026-08-27 20:20:30 +0200

    chore(text): remove noisy test

--- tests/models/md/test_markdown_str.py
+++ tests/models/md/test_markdown_str.py
@@ -373,10 +373,7 @@ class TestGetExtent(unittest.TestCase):
         text = mdformat.text("# Title\nParagraph on line 2.\n\n\n## *More* `heading`")
         lines = text.splitlines()
         expected = len(lines)
-        print("")
         lines = text.splitlines()
-        for i, line in enumerate(lines):
-            print(f"[{i}] {line}")
         result = MarkdownStr.get_extent(text)
         self.assertEqual(result, expected)
 
